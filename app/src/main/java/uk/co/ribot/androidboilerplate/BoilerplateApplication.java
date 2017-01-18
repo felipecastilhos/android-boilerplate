420f7c65212f54419938484799e0286ec8f1e46e
Felipe Castilhos <felipecastilhos@gmail.com>
2017-01-18 17:27:50 -0200

    clean up dead code

--- app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
@@ -37,9 +37,4 @@ public class BoilerplateApplication extends Application  {
         }
         return mApplicationComponent;
     }
-
-    // Needed to replace the component with a test specific one
-    public void setComponent(ApplicationComponent applicationComponent) {
-        mApplicationComponent = applicationComponent;
-    }
 }
